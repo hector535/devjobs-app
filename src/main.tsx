4dda148649dcdc3076323d945e5a39a3f0028b86
Hector Figuereo <hectorfiguereo26@gmail.com>
2023-08-30 00:37:38 -0400

    Set routes to the RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { RouterProvider, createHashRouter } from "react-router-dom";
+import { Root } from "./routes/roots";
+import { Jobs } from "./routes/jobs/jobs";
 import "../sass/style.scss";
 
+const router = createHashRouter([
+  {
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Jobs />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
