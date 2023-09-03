d04f9d9f47d7d9cfb3557af340af762b95d16d5c
Hector Figuereo <hectorfiguereo26@gmail.com>
2023-09-03 01:51:18 -0300

    Create NoResultsMessage component

---
+++ src/components/NoResultsMessage/NoResultsMessage.tsx
@@ -0,0 +1,11 @@
+import { ReactComponent as SadFaceSVG } from "@/assets/sad-face.svg";
+import style from "./NoResultsMessage.module.scss";
+
+export const NoResultsMessage = () => {
+  return (
+    <div className={style.no_results_message_container}>
+      <SadFaceSVG />
+      <h1>Sorry, we couldn't find any results</h1>
+    </div>
+  );
+};
