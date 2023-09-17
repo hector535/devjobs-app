790819d2753d0940cb9d69d8c9ce87bece920578
Hector Figuereo <hectorfiguereo26@gmail.com>
2023-09-17 01:10:58 -0400

    Fix bug when user type invalid ID on job details page

--- src/components/error-view/error-view.tsx
+++ src/components/error-view/error-view.tsx
@@ -1,9 +1,17 @@
 import clsx from "clsx";
 import { type ErrorViewProps } from "./error-view.types";
 import style from "./error-view.module.scss";
+import { useNavigate } from "react-router-dom";
 
 export const ErrorView = (props: ErrorViewProps) => {
   const { fullscreen, resetErrorBoundary } = props;
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/jobs");
+    resetErrorBoundary();
+  };
   return (
     <div
       className={clsx(style.error_container, {
@@ -13,7 +21,7 @@ export const ErrorView = (props: ErrorViewProps) => {
       <h1>500</h1>
       <h2>🔥Internal Server Error🔥</h2>
       <p>Sorry, something went wrong :(</p>
-      <button onClick={resetErrorBoundary}>Refresh</button>
+      <button onClick={handleClick}>Go to the main page</button>
     </div>
   );
 };
